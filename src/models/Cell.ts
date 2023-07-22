91fff2ab28356b0fa70d551f195618cad9c5a9ac
Aldar <aldarkonov21@gmail.com>
2023-07-22 14:33:39 +0300

    feat: add board and cell models

---
+++ src/models/Cell.ts
@@ -0,0 +1,31 @@
+import { v4 as uuid4 } from "uuid";
+
+import { Board } from "./Board";
+import { Colors } from "./Colors";
+import { Figure } from "./figures";
+
+export class Cell {
+  readonly x: number;
+  readonly y: number;
+  readonly color: Colors;
+  figure: Figure | null;
+  board: Board;
+  available: boolean;
+  id: string;
+
+  constructor(
+    board: Board,
+    x: number,
+    y: number,
+    color: Colors,
+    figure: Figure | null
+  ) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.figure = figure;
+    this.board = board;
+    this.available = false;
+    this.id = uuid4();
+  }
+}
