3ed0c1aef43b60d4db68dc60fa111999b1291141
Aldar <aldarkonov21@gmail.com>
2023-08-10 22:44:20 +0300

    refactor: [BE] update user model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -3,11 +3,11 @@ import { Schema, model } from "mongoose";
 
 const userSchema = Schema(
   {
-    name: { type: "String", required: true },
-    email: { type: "String", unique: true, required: true, unique: true },
-    password: { type: "String", required: true },
+    name: { type: String, required: true },
+    email: { type: String, unique: true, required: true, unique: true },
+    password: { type: String, required: true },
     image: {
-      type: "String",
+      type: String,
       required: true,
       default:
         "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
