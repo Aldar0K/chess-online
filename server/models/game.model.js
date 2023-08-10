435a7f41a642c5da50a920109e00e18c60ed3938
Aldar <aldarkonov21@gmail.com>
2023-08-10 22:41:42 +0300

    feat: [BE] add game model

--- server/models/game.model.js
+++ server/models/game.model.js
@@ -2,12 +2,28 @@ import { Schema, model } from "mongoose";
 
 const gameSchema = Schema(
   {
-    pgn: { type: "String", required: false },
-    white: { type: Schema.Types.ObjectId, ref: "User" },
-    black: { type: Schema.Types.ObjectId, ref: "User" },
-    winner,
-    endReason,
-    host: { type: Schema.Types.ObjectId, ref: "User" },
+    pgn: { type: String, required: false },
+    white: { type: Schema.Types.ObjectId, ref: "User", required: false },
+    black: { type: Schema.Types.ObjectId, ref: "User", required: false },
+    winner: { type: String, enum: ["white", "black", "draw"] },
+    endReason: {
+      type: String,
+      enum: [
+        "draw",
+        "checkmate",
+        "stalemate",
+        "repetition",
+        "insufficient",
+        "abandoned",
+      ],
+    },
+    host: { type: Schema.Types.ObjectId, ref: "User", required: false },
+    code: { type: String, required: false },
+    unlisted: { type: Boolean, required: false },
+    timeout: { type: Number, required: false },
+    observers: [{ type: Schema.Types.ObjectId, ref: "User", required: false }],
+    startedAt: { type: Number, required: false },
+    endedAt: { type: Number, required: false },
   },
   { timestaps: true }
 );
