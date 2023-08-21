8f591b165692761a7bd77ad4d39b79ecf95f9f44
Aldar <aldarkonov21@gmail.com>
2023-08-21 19:24:30 +0300

    feat: [FE] update user card

--- client/src/entities/user/ui/user-card/UserCard.tsx
+++ client/src/entities/user/ui/user-card/UserCard.tsx
@@ -1,50 +1,51 @@
-import { Avatar, Box, Spinner, Text } from '@chakra-ui/react';
-import { FC } from 'react';
+import { Avatar, Box, Text } from "@chakra-ui/react";
+import { FC } from "react";
 
-import { chatModel } from 'entities/chat';
-import { User } from '../../model/types';
+import { User } from "../../model/types";
 
 interface UserCardProps {
   user: User;
   onClose: () => void;
 }
 
-const UserCard: FC<UserCardProps> = ({ user, onClose }) => {
-  const { mutate: accessChatMutate, isLoading: accessChatLoading } =
-    chatModel.useAccessChatMutation();
-
+const UserCard: FC<UserCardProps> = ({ user }) => {
   const handleClick = async () => {
-    accessChatMutate(user._id, { onSuccess: onClose });
+    console.log("click");
   };
 
   return (
     <Box
-      display='flex'
-      w='100%'
+      display="flex"
+      w="100%"
       px={3}
       py={2}
-      bg='#E8E8E8'
-      alignItems='center'
-      borderRadius='lg'
-      color='black'
-      cursor='pointer'
-      transition='all .2s ease'
-      userSelect='none'
+      bg="#E8E8E8"
+      alignItems="center"
+      borderRadius="lg"
+      color="black"
+      cursor="pointer"
+      transition="all .2s ease"
+      userSelect="none"
       _hover={{
-        background: '#38B2AC',
-        color: 'white'
+        background: "#38B2AC",
+        color: "white",
       }}
       onClick={handleClick}
     >
-      <Avatar mr='2' size='sm' cursor='pointer' name={user.name} src={user.image} />
-      <Box mr='auto'>
+      <Avatar
+        mr="2"
+        size="sm"
+        cursor="pointer"
+        name={user.name}
+        src={user.image}
+      />
+      <Box mr="auto">
         <Text>{user.name}</Text>
-        <Text fontSize='xs'>
+        <Text fontSize="xs">
           <b>Email : </b>
           {user.email}
         </Text>
       </Box>
-      {accessChatLoading && <Spinner display='flex' />}
     </Box>
   );
 };
